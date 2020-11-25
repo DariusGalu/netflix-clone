99cea7a41fe27a5fee5c8eed52b8e866f2392f6b
DariusGalu <galu.darius77@gmail.com>
2020-11-25 11:38:46 +0200

    init

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -67,7 +67,7 @@ export default function Signin() {
                     </Form.Text>
 
                     <Form.TextSmall>
-                        Thsis page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
                     </Form.TextSmall>
                 </Form>
             </HeaderContainer>
